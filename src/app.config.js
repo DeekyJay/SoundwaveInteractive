b67ed1d62cb8b5da921aa92da55872e105b1709f
Derek Jensen <derek.jensen5@gmail.com>
2017-06-30 21:56:26 -0600

    auth

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,6 @@
 
 // const DEV_API_PATH = 'http://localhost:3000/'
-const DEV_API_PATH = 'http://soundwave.deek.io/api/v1'
-const PROD_API_PATH = 'http://soundwave.deek.io/api/v1'
+const DEV_API_PATH = 'https://soundwave.deek.io/api/v1'
+const PROD_API_PATH = 'https://soundwave.deek.io/api/v1'
 const API_BASE_URL =  process.env.NODE_ENV === 'development' ? DEV_API_PATH : PROD_API_PATH
 export default {'API_BASE_URL': API_BASE_URL};
